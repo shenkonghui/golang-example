0c63ceb4fdd164b686b5b4f20a9a055d02937ee7
shenkonghui <18767151789@163.com>
2020-09-29 10:58:35 +0800

    增加 pprof demo

---
+++ PProf-demo/README.md
@@ -0,0 +1,7 @@
+## 浏览器查看
+打开浏览器访问 http://localhost:6060/debug/pprof/
+
+## 交互
+go tool pprof http://localhost:6060/debug/pprof/profile\?seconds\=60
+
+打印堆栈图: `PDF`
---
+++ PProf-demo/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+	"time"
+)
+
+var datas []string
+
+func Add(str string) string {
+	data := []byte(str)
+	sData := string(data)
+	datas = append(datas, sData)
+	return sData
+}
+
+func main() {
+	if true {
+		go func() {
+			for {
+				log.Println(Add("https://github.com/EDDYCJY"))
+				time.Sleep(time.Millisecond * 10)
+			}
+		}()
+		runtime.GOMAXPROCS(1)               // 限制 CPU 使用数，避免过载
+		runtime.SetMutexProfileFraction(1)  // 开启对锁调用的跟踪
+		runtime.SetBlockProfileRate(1)      // 开启对阻塞操作的跟踪
+		http.ListenAndServe("0.0.0.0:6060", nil)
+	}
+
+}
